4a8517c6c3f546f4c398c3268494fde1bd984334
jishnucm <jishnucm55@gmail.com>
2022-04-18 23:46:28 +0530

    new project

--- online_bookstore_project/settings.py
+++ online_bookstore_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^@!wd!los_7)me_+jz0$8bt79$!th$^zfk4#!x7=rq1v8_j6mn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookstore_user'
+    
 ]
 
 MIDDLEWARE = [
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
